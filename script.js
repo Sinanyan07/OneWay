f4ee2a386f65f87b5142b063eeeedbea7a2b76c2
Biainili <barkhudaryan005@gmail.com>
2024-04-09 17:54:41 +0400

    InputContainer

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function changeCode(code) {
     document.getElementById('dropdownMenuButton').innerText = code;
 }
+
+
+function for_see_password_type(id) {
+    const passwordInput = document.getElementById(id);
+    passwordInput.type === "password" ? passwordInput.type = 'text' : passwordInput.type = 'password';
+}
