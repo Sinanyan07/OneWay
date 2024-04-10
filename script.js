52fd61cc7dccf194b5794bbc609d5143fe669e0a
Biainili <barkhudaryan005@gmail.com>
2024-04-10 15:06:05 +0400

    changesFor1440px

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function changeCode(code) {
-    document.getElementById('dropdownMenuButton').innerText = code;
+function changeCode(code, Id) {
+    document.getElementById(Id).innerText = code;
 }
 
 
 function for_see_password_type(id) {
     const passwordInput = document.getElementById(id);
     passwordInput.type === "password" ? passwordInput.type = 'text' : passwordInput.type = 'password';
+}
+
+
+{
+    function listenerChangeRadio(event) {
+        const companyNameContainer = document.getElementById("company_name_container");
+        if (event.target.value === "individual") {
+            companyNameContainer.style.display = "none";
+        } else {
+            companyNameContainer.style.display = "block";
+        }
+    }
 }
